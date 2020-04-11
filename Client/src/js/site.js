a5664ae99e94ad24e810a0c74d7d1ec3e33ae5da
Jorge <jorge@pop-os.localdomain>
2020-04-10 23:48:42 -0400

    Divisão dos pacotes

--- Client/src/js/site.js
+++ Client/src/js/site.js
@@ -1,17 +1,10 @@
-﻿// JS Dependencies: Bootstrap & JQuery
-import 'bootstrap';
-import 'jquery';
-// Using the next two lines is like including partial view _ValidationScriptsPartial.cshtml
-import 'jquery-validation';
-import 'jquery-validation-unobtrusive';
+﻿import 'bootstrap/dist/css/bootstrap.css';
+import '../css/site.css';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic/build/ckeditor';
+// import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
-// CSS Dependencies: Bootstrap
-import 'bootstrap/dist/css/bootstrap.css';
-
-// Custom JS imports
-// ... none at the moment
-
-// Custom CSS imports
-import '../../../wwwroot/css/site.css';
-
-console.log('The \'site\' bundle has been loaded!');
+// ClassicEditor
+//     .create(document.querySelector('#editor'))
+//     .then()
+//     .catch();
+console.log('Pacote do site carregado')
---
+++ Client/src/js/validation.js
@@ -0,0 +1,5 @@
+import 'jquery';
+import 'jquery-validation';
+import 'jquery-validation-unobtrusive';
+
+console.log('Pacote de validação carregado')
